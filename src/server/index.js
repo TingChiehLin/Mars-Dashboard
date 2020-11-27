a4ac756872c390158f2e9271f17d758128d2f279
Jay Lin <cooloojayoo@gmail.com>
2020-11-27 16:16:23 +1100

    Get data error

--- src/server/index.js
+++ src/server/index.js
@@ -41,13 +41,11 @@ app.get('/roverimage/:apod', async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-header', 'Content-Type, Authorization');
-    next();
 
     try {        
-        let data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${API_KEY}`)
+        let data = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/:apod/photos?sol=1000&api_key=${API_KEY}`)
             .then(res => res.json())
             res.send(data.photos)
-            console.log(res.body.rover);
     } catch (err) {
         console.log('error:', err);
     }
